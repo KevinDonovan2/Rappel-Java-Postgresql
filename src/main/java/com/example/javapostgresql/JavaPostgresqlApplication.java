3eba941e4aacdd1254444ab31c3f46bd79861e07
Donovan <hei.donovan.2@gmail.com>
2023-11-17 11:43:42 +0300

    feat : init projet

---
+++ src/main/java/com/example/javapostgresql/JavaPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.javapostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPostgresqlApplication.class, args);
+	}
+
+}
